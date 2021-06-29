4b0b97d119bdf4261ab8860691b8ead8dacf7c1b
zhenggshuai <zhengsuhai@gmail.com>
2021-06-29 22:26:54 +0800

    rm apps/cmdb_bak

--- apps/cmdb_bak/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CmdbConfig(AppConfig):
-    name = 'apps.cmdb'
--- apps/cmdb_bak/router.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register('v1/cmdb/instances', views.CMDBServerModelViewSet)
-router.register('v1/cmdb/table-classify', views.ClassifyModelViewSet)
-router.register('v1/cmdb/table-field', views.TableFieldModelViewSet)
-router.register('v1/cmdb/table-data', views.TableDataModelViewSet)
-
